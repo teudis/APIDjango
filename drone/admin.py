6a5751ed7cbca97d575dbd08a75395f8f8af48db
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2022-06-04 07:45:03 -0400

    Modify URL for show Image

--- drone/admin.py
+++ drone/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Drone, Medication, DispacherDrone
 
 # Register your models here.
+admin.site.register(Drone)
+admin.site.register(Medication)
+admin.site.register(DispacherDrone)
