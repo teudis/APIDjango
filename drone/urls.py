26522d778f75890a4238923f6eb35d5690dfe823
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2022-06-04 07:39:02 -0400

    Modify Settings adn URL

--- drone/urls.py
+++ drone/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import ListDrone, DetailDrone
+
 urlpatterns = [
     path('<int:pk>/', DetailDrone.as_view()),
     path('', ListDrone.as_view()),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
