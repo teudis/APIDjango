11204002c39d56e05732fc35e1810a38b34e3159
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2022-06-04 08:27:35 -0400

    Add Drone List State Loading

--- drone/urls.py
+++ drone/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListDrone, DetailDrone
+from .views import ListDrone, DetailDrone, ListDroneLoading
 
 urlpatterns = [
     path('<int:pk>/', DetailDrone.as_view()),
     path('', ListDrone.as_view()),
+    path('loading', ListDroneLoading.as_view()),
     ]
--- drone/views.py
+++ drone/views.py
@@ -2,10 +2,17 @@ from rest_framework import generics
 from .models import Drone
 from .serializers import DroneSerializer
 
+#Show Drone List And Add Drone
 class ListDrone(generics.ListCreateAPIView):
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
 
+# Details about a Drone
 class DetailDrone(generics.RetrieveAPIView):
     queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+
+#Drone LIST STATE LOADING
+class ListDroneLoading(generics.ListAPIView):
+    queryset = Drone.objects.filter(state="LOADING")
     serializer_class = DroneSerializer
