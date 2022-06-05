290cea9037559421cce10097edc442040d42dffe
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2022-06-05 07:52:47 -0400

    List Drone Dispatcher

--- drone/urls.py
+++ drone/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import ListDrone,CreateDrone, DetailDrone, ListDroneLoading, DispatchDrone
+from .views import ListDrone,CreateDrone, DetailDrone, ListDroneLoading, DispatchDrone,ListDispatchDrone , BatteryDrone
 
 urlpatterns = [
     path('<int:pk>/', DetailDrone.as_view(), name='drone_detail'), # details drone
     path('drone/list', ListDrone.as_view(), name='list_drone'), # all drone list
     path('drone/loading', ListDroneLoading.as_view(), name='list_drone_loading'), # drone list loading
     path('drone/create', CreateDrone.as_view(), name='create_drone'), # create drone
-    path('dispatch', DispatchDrone.as_view()),
+    path('drone/<int:pk>/check', BatteryDrone.as_view(), name='check_battery_drone'), # Check Battery drone
+    path('dispatch/create', DispatchDrone.as_view(), name="create_dispatch"),
+    path('dispatch/list', ListDispatchDrone.as_view(), name="list_dispatch"),
     ]
--- drone/views.py
+++ drone/views.py
@@ -1,8 +1,6 @@
 from rest_framework import generics
-from .models import Drone ,DispacherDrone
+from .models import Drone, DispacherDrone
 from .serializers import DroneSerializer, DispatcherDroneSerializer
-
-
 
 class CreateDrone(generics.CreateAPIView):
     serializer_class = DroneSerializer
@@ -17,12 +15,21 @@ class DetailDrone(generics.RetrieveAPIView):
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
 
+class BatteryDrone(generics.RetrieveAPIView):
+    queryset = Drone.objects.only('battery_capacity')
+    serializer_class = DroneSerializer
+
 #Drone LIST STATE LOADING
 class ListDroneLoading(generics.ListAPIView):
     queryset = Drone.objects.filter(state="LOADING")
     serializer_class = DroneSerializer
 
-
 #Dispacher DRONE
 class DispatchDrone(generics.CreateAPIView):
+    serializer_class = DispatcherDroneSerializer
+
+
+#Dispacher DRONE
+class ListDispatchDrone(generics.ListAPIView):
+    queryset = DispacherDrone.objects.all()
     serializer_class = DispatcherDroneSerializer
